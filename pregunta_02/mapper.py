186a09f53164fd7382e97859790d57ebc159a37a
javarelaf <101435311+javarelaf@users.noreply.github.com>
2022-06-02 19:41:36 -0500

    Add files via upload

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/python3
+
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+        
+            #
+            # escribe al flujo estandar de salida
+            #
+            sys.stdout.write("{}\t{}\n".format(line.split(',')[3],line.split(',')[4]))
