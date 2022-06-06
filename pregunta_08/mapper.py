6e630664640d0c5a46d9f5b0f70c7b565625fcb8
javarelaf <101435311+javarelaf@users.noreply.github.com>
2022-06-05 22:03:50 -0500

    Add files via upload

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+#! /usr/bin/python3
+
+#
+# Se inicializa
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+            #
+            # escribe al flujo estandar de salida
+            #
+            sys.stdout.write("{},{}".format(line.split('   ')[0],line.split('   ')[2]))
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,49 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#! /usr/bin python3
+
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    cuenta=0
+    suma=0
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.split(",")
+        val = int(val)
+        
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+         suma += val
+         cuenta +=1
+         promedio=suma/cuenta
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma,promedio))
+                
+            curkey = key
+            suma = val
+            cuenta = 1 
+            promedio = suma/cuenta
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey,suma,promedio))
