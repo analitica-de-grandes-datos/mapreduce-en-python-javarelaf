ef72f5c25abfb1f8134ecb5f9bf2c23053aa40a7
javarelaf <101435311+javarelaf@users.noreply.github.com>
2022-06-06 21:35:15 -0500

    Add files via upload

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+#! /usr/bin/python3
+
+#
+# Se inicializa
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+        
+            #
+            # escribe al flujo estandar de salida
+            #
+            sys.stdout.write("{}\t1\n".format(line.split('   ')[0]))
