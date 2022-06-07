7e6906a44d8c20af3538ca241a9011268c93df33
javarelaf <101435311+javarelaf@users.noreply.github.com>
2022-06-06 21:57:51 -0500

    Add files via upload

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+#! /usr/bin/python3
+
+#
+# Se inicializa
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+        
+            #
+            # escribe al flujo estandar de salida
+            #
+            
+            sys.stdout.write("{}\t{}\t{}\n".format(str(line.split('   ')[0]),str(line.split('   ')[1]),int(line.split('   ')[2])))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,51 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+#! /usr/bin python3
+
+import sys
+import bisect
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    registros=[]
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+        key,fecha, val = line.split("\t")
+        val = int(val)
+        
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            bisect.insort(registros,(val,fecha,key))
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                for n in registros:
+                 sys.stdout.write("{}   {}   {}\n".format(n[2],n[1],n[0]))
+                # inicializamos
+                registros=[]
+                
+
+            curkey = key
+            bisect.insort(registros,(val,fecha,key))
+                        
+    
+    for n in registros:            
+     sys.stdout.write("{}   {}   {}\n".format(n[2],n[1],n[0]))
+     
