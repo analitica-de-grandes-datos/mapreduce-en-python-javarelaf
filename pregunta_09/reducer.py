e5d2f971191af5a74e134459321b58f6098d7dfe
javarelaf <101435311+javarelaf@users.noreply.github.com>
2022-06-06 22:04:15 -0500

    Add files via upload

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+#! /usr/bin python3
+
+import sys
+import bisect
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    registros=[]
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+        key,fecha, val = line.split("\t")
+        val = int(val)
+        bisect.insort(registros,(val,fecha,key))
+        
+    for n in range(0,6):            
+     sys.stdout.write("{}   {}   {}\n".format(registros[n][2],registros[n][1],registros[n][0]))
+     
