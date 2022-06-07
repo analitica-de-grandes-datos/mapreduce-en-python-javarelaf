2506d7c5fcf0a8402c09114f57d89e174692b98c
javarelaf <101435311+javarelaf@users.noreply.github.com>
2022-06-06 21:00:00 -0500

    Add files via upload

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,34 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+#! /usr/bin/python3
+
+#
+# Se inicializa
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    
+    for line in sys.stdin:
+        
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+        pares=list()
+        line=line.replace('\n','')
+        index,letras = line.split('\t')
+        letras=letras.split(',')
+        for letra in letras:
+            pares.append([index,letra])
+            #
+            # escribe al flujo estandar de salida
+            #
+        for par in pares:
+            sys.stdout.write(str(par[1])+'\t'+str(par[0])+'\n')
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,61 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+#! /usr/bin python3
+
+import sys
+import bisect
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    keys=[]
+    total=[]
+    total2=[]
+    total3=[]
+    total4=''
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            total.append(val)
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                for num in total:
+                    bisect.insort(total2,num)
+                total3=[str(x) for x in total2]
+                total4 = ','.join(total3)
+                sys.stdout.write("{}\t{}\n".format(curkey, total4))
+                # inicializamos
+                total=[]
+                total2=[]
+                total3=[]
+                total4 = ''
+
+            curkey = key
+            total.append(val)
+                        
+    for num in total:
+                    bisect.insort(total2,num)
+    total3=[str(x) for x in total2]
+    total4 = ','.join(total3)
+                
+    sys.stdout.write("{}\t{}\n".format(curkey, total4))
