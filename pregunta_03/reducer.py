ec84a339754dde70e51e3dafc573e1d1eb9bde70
javarelaf <101435311+javarelaf@users.noreply.github.com>
2022-06-06 21:28:24 -0500

    Add files via upload

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,20 @@
 #
-# >>> Escriba el codigo del reducer a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/python3
+registros=[]
+import sys
+import bisect
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+        key,val=line.split('\t')  
+        val=int(val)
+        bisect.insort(registros,(val,key))
+        
+    for n in registros: 
+      sys.stdout.write("{},{}\n".format(n[1],n[0]))
